68289e9863c74530cf04291a50628c2003837828
yousef69mr <84923007+yousef69mr@users.noreply.github.com>
2022-12-23 19:35:18 +0200

    1st version

---
+++ src/Data/data.js
@@ -0,0 +1,51 @@
+import baliImg from "../assets/img/bali.jpg";
+
+export const Data = [{
+    id: 1,
+    image: baliImg,
+    title: 'Bali Island',
+    location: 'Indonesia',
+    grade: 'Cultural Relax',
+    fees: '$500',
+    description: 'Indonesian Island'
+}, {
+    id: 2,
+    image: baliImg,
+    title: 'Bali Island',
+    location: 'Indonesia',
+    grade: 'Cultural Relax',
+    fees: '$500',
+    description: 'Indonesian Island'
+}, {
+    id: 3,
+    image: baliImg,
+    title: 'Bali Island',
+    location: 'Indonesia',
+    grade: 'Cultural Relax',
+    fees: '$500',
+    description: 'Indonesian Island'
+}, {
+    id: 4,
+    image: baliImg,
+    title: 'Bali Island',
+    location: 'Indonesia',
+    grade: 'Cultural Relax',
+    fees: '$500',
+    description: 'Indonesian Island'
+}, {
+    id: 5,
+    image: baliImg,
+    title: 'Bali Island',
+    location: 'Indonesia',
+    grade: 'Cultural Relax',
+    fees: '$500',
+    description: 'Indonesian Island'
+}, {
+    id: 6,
+    image: baliImg,
+    title: 'Bali Island',
+    location: 'Indonesia',
+    grade: 'Cultural Relax',
+    fees: '$500',
+    description: 'Indonesian Island'
+}]
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./home.css";
 import video1 from "../../assets/video/Egypt _PeopletoPeople.mp4";
 import video2 from "../../assets/video/ThisisEgypt.mp4";
@@ -10,7 +10,14 @@ import { FaTripadvisor } from "react-icons/fa";
 import { TbApps } from "react-icons/tb";
 import { BsListTask } from "react-icons/bs";
 
+import aos from "aos";
+import "aos/dist/aos.css";
+
 const Home = () => {
+  useEffect(() => {
+    aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <section className="home">
       <div className="overlay"></div>
@@ -18,11 +25,15 @@ const Home = () => {
 
       <div className="homeContent container">
         <div className="textDiv">
-          <span className="smallText">Our Packages</span>
-          <h1 className="homeTitle">Search your Holiday</h1>
+          <span data-aos="fade-up" className="smallText">
+            Our Packages
+          </span>
+          <h1 data-aos="fade-up" className="homeTitle">
+            Search your Holiday
+          </h1>
         </div>
 
-        <div className="cardDiv grid">
+        <div data-aos="fade-up" className="cardDiv grid">
           <div className="destinationInput">
             <label htmlFor="city">Search your destination</label>
             <div className="input flex">
@@ -52,7 +63,7 @@ const Home = () => {
             <span>More Filters</span>
           </div>
         </div>
-        <div className="homeFooterIcons flex">
+        <div data-aos="fade-up" className="homeFooterIcons flex">
           <div className="rightIcons">
             <FiFacebook className="icon" />
             <AiOutlineInstagram className="icon" />
